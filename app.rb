3c2e97740e5d4891f20ce756f8b56c05fad73a3e
“Mhaventhan” <mhaventhan@gmail.com>
2018-08-22 20:37:51 +0100

    comments for the seven restful routes

--- app.rb
+++ app.rb
@@ -7,41 +7,59 @@ require 'sinatra/reloader' if development?
 
 
 # first route
+# HTTP Verb = GET
+# Controller Action = index
 # display a list of all the resources
 get '/Pets' do
   'get all Pets'
 end
 
 # second route
+# HTTP Verb = GET
+# Controller Action = new
 # returns a HTML form for creating a new source
+
 get "/Pets/new" do
   "get new pet form"
 end
  # third route
+ # HTTP Verb = POST
+ # Controller Action = create
  # to create a new ressource
 post '/Pets' do
   "adding a new pet"
 end
  # fourth route
+ # HTTP Verb = GET
+ # Controller Action = show
  # to display a specific resource
 get '/Pets/:id' do
   id = params[:id]
   "get a single Pet #{id}"
 end
+
 # fifth route
+# HTTP Verb = GET
+# Controller Action = edit
 # returns a HTML form for editing a resource
+
 get '/Pets/:id/edit' do
   id = params[:id]
   "Get edit pet form #{id}"
 end
 # sixth route
+# HTTP Verb = PATCH/PUT
+# Controller Action = update
 # to update a specific resource
 put '/Pets/:id' do
   id = params[:id]
   "updates a pet #{id}"
 end
+
 # seventh route
-# to delete a specific resource 
+# HTTP Verb = DELETE
+# Controller Action = destroy
+# to delete a specific resource
 delete '/Pets/:id' do
   id = params[:id]
   "delete a pet #{id}"
